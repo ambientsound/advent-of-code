585ad2e38d91853cadd519fd67b68a5a247a0247
Kim Tore Jensen <kimtjen@gmail.com>
2020-12-02 11:57:02 +0100

    solve 2.1

---
+++ 2020/2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	lib2 "github.com/ambientsound/aoc/2/lib"
+)
+
+func main() {
+	raw, _ := ioutil.ReadFile("/Users/kimt/src/aoc/2020/2/input.txt")
+	policies := lib2.ReadFile(raw)
+	for _,policy := range policies{
+		if policy.Valid() {
+			fmt.Printf("valid: %s\n", policy.Password)
+		} else {
+			fmt.Printf("invalid: %s\n", policy.Password)
+		}
+	}
+}
